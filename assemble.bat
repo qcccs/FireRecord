d1f61241f9009b60a90d770d0b9760af02fcd684
David Westgate <davidjwestgate@gmail.com>
2013-04-21 14:14:40 -0300

    Update assemble.bat
    winrar support

--- assemble.bat
+++ assemble.bat
@@ -1,7 +1,9 @@
 if exist "C:\Program Files\7-Zip\7z.exe" (
 	"C:\Program Files\7-Zip\7z.exe" a FireRecord.zip chrome defaults chrome.manifest install.rdf
-) else (
+) else if exist "C:\Program Files (x86)\7-Zip\7z.exe" (
 	"C:\Program Files (x86)\7-Zip\7z.exe" a FireRecord.zip chrome defaults chrome.manifest install.rdf
+) else (
+	"C:\Program Files\WinRAR\rar.exe" a FireRecord.zip chrome defaults chrome.manifest install.rdf
 )
 move FireRecord.zip FireRecord.xpi
 start "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" FireRecord.xpi
